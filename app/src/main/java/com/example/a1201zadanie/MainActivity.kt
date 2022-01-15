d77bd4cfd592dee761d85abcc5f8ff83dfdf2892
Reilo258 <olivier258@wp.pl>
2022-01-15 22:18:49 +0100

    Zrobione

--- app/src/main/java/com/example/a1201zadanie/MainActivity.kt
+++ app/src/main/java/com/example/a1201zadanie/MainActivity.kt
@@ -3,7 +3,9 @@ package com.example.a1201zadanie
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -42,6 +44,27 @@ class MainActivity : AppCompatActivity() {
                 findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.t34);
             else if(licznik == 4)
                 findViewById<ImageView>(R.id.obraz).setImageResource(R.drawable.tirpitz);
+        }
+
+        findViewById<Button>(R.id.rotprawbtn).setOnClickListener {
+            var rotacja = findViewById<EditText>(R.id.rotacja).text;
+            findViewById<ImageView>(R.id.obraz).rotation = rotacja.toString().toFloat();
+        }
+
+        findViewById<Button>(R.id.rotlewobtn).setOnClickListener {
+            var rotacja = "-" + findViewById<EditText>(R.id.rotacja).text;
+            findViewById<ImageView>(R.id.obraz).rotation = rotacja.toString().toFloat();
+        }
+
+        findViewById<Button>(R.id.skalabtn).setOnClickListener {
+            var skala = findViewById<EditText>(R.id.skala).text;
+            findViewById<ImageView>(R.id.obraz).scaleX = skala.toString().toFloat();
+            findViewById<ImageView>(R.id.obraz).scaleY = skala.toString().toFloat();
+        }
+
+        findViewById<Button>(R.id.przezbtn).setOnClickListener {
+            var przezroczystosc = findViewById<EditText>(R.id.przezr).text;
+            findViewById<ImageView>(R.id.obraz).alpha = przezroczystosc.toString().toFloat();
         }
     }
 }
